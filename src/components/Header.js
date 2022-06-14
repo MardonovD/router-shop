89331fc372dc2c92861bc6d2c7c0014b7c7cb5ef
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-14 10:03:00 +0300

    Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="header">
@@ -8,16 +8,27 @@ const Header = () => {
           <div className="row">
             <div className=" header-min d-flex align-items-center p-2 col-12 d-flex justify-content-between  ">
               <span>Mardonov</span>
-              <div className="nav">
+              <div className="nav d-flex align-content-center">
                 <ul className="d-flex gap-5  ">
                   <li className="nav-item">
-                    <a href="#">Home</a>
+                    <Link to="/" href="#">
+                      Home
+                    </Link>
                   </li>
                   <li className="nav-item">
-                    <a href="#">About</a>
+                    <Link to="shop" href="#">
+                      Shop
+                    </Link>
                   </li>
                   <li className="nav-item">
-                    <a href="#">Contact</a>
+                    <Link to="contact" href="#">
+                      Contact
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="about" href="#">
+                      About
+                    </Link>
                   </li>
                 </ul>
               </div>
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,22 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import Header from "../components/Header";
+import About from "../components/About";
+import Shop from "../components/Shop";
+import Contact from "../components/Contact";
 
 const Home = () => {
   return (
     <div className="home">
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/shop" element={<Shop />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
